c6c87246ab006746c7edc07601bb97bd7fbabfbe
Lisa Cormier <lcormier5@hotmail.com>
2021-09-28 16:55:24 -0600

    initial commit, adding button component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "components/DayListItem.scss";
+const classnames = require('classnames');
+
+export default function DayListItem(props) {
+  const dayListClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots===0
+ });
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayListClass}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots}</h3>
+    </li>
+  );
+}
