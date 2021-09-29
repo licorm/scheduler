dcf903443ca36e1b669365d272c826805ec37a33
Lisa Cormier <lcormier5@hotmail.com>
2021-09-28 18:40:22 -0600

    adding functional daylist component

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DayListItem from './DayListItem';
+
+export default function DayList(props) {
+  const list = props.days.map((obj) => {
+    return (
+    <DayListItem
+    name={obj.name}
+    spots={obj.spots}
+    selected={obj.name === props.day}
+    setDay={props.setDay}
+    />
+    )
+  })
+  return (<ul>{list}</ul>);
+}
