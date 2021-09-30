335182e1462f23a1283d99af1d87452c974912e2
Lisa Cormier <lcormier5@hotmail.com>
2021-09-30 17:08:43 -0600

    adding axios request toadd days data

--- src/components/Appointments/Form.js
+++ src/components/Appointments/Form.js
@@ -32,7 +32,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave(studentName, interviewer)}>Save</Button>
+          <Button confirm onClick={props.onSave(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
