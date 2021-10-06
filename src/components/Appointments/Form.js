5f2d63cf1d27fe3cba6060be9433df0fd012a326
Lisa Cormier <lcormier5@hotmail.com>
2021-10-06 16:06:20 -0600

    adding cypress test framework and first test for navigation

--- src/components/Appointments/Form.js
+++ src/components/Appointments/Form.js
@@ -6,16 +6,27 @@ export default function Form(props) {
   //setting states
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   //function to reset the states
-  const reset = () => { return setName(""), setInterviewer(null) }
+  const reset = () => { 
+    return setName(""), setInterviewer(null) 
+  }
+
   //function to send to cancel button which calls the props and reset functions
   const cancel = () => {
     return (props.onCancel(), reset())
   }
-  
-   console.log(name, interviewer)
-console.log("form props:", props)
+
+ //function to make sure that the user can't submit if they don't enter their name
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -26,19 +37,20 @@ console.log("form props:", props)
             value={name}
             onChange={(event) => setName(event.target.value)}
             type="text"
-            placeholder="Enter your name here"
+            placeholder="Enter Student Name"
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
          <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
   )
 };
 
-//<Button confirm onClick={props.onSave(name, interviewer)}>Save</Button>
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -22,8 +22,8 @@ export default function DayListItem(props) {
 
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayListClass}>
-      <h2 className="text--regular">{props.name}</h2>
+    <li onClick={() => props.setDay(props.name)} className={dayListClass} data-testid="day">
+      <h2 className="text--regular" >{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
